c52bdd68c6a5584abe1ccabadb5745f84eb42dac
Silas  Owu <silas.owu@amalitech.com>
2023-01-31 15:19:53 -0800

    Galleria slideshow pushed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Routes, Route} from "react-router-dom"
+import Slideshow from './component/Slideshow/Slideshow';
+import Homepage from './component/Homepage';
+// import Gallery from './component/Gallery/Gallery';
+
+function App() {
+  return (
+    <Routes>
+        <Route path='/' element={<Homepage/>} />
+        <Route path='Slideshow' element={<Slideshow/>}/>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/component/Slideshow/Slideshow.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Slideshow.css'
+// import "./Tablet/tablet.css";
+import Header from './Header'
+import Content from './Content'
+
+
+
+const Slideshow = () => {
+  return (
+    <div className='slideshow'>
+        <Header/>
+        <Content/>
+    </div>
+  )
+}
+
+export default Slideshow
